aacbd08e2105616b45e059fdd2a45cb2ce9f9488
Tom Booth <tombeezle@gmail.com>
2022-09-15 11:57:26 -0700

    redid the candy section based off marty example for reference

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // import functions and grab DOM elements
+import { candies } from './candies-data.js';
+import { renderCandy } from './render-candy.js';
 
+const candyList = document.getElementById('candy-list');
+
+function displayCandies() {
+    candyList.innerHTML = '';
+
+    for (let candy of candies) {
+        const candyEl = renderCandy(candy);
+        candyList.append(candyEl);
+    }
+}
+
+displayCandies();
 // initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
---
+++ candies-data.js
@@ -0,0 +1,17 @@
+export const candies = [
+    {
+        name: 'Twix',
+        flavor: 'chocolate caramel',
+        type: 'candy bar'
+    }
+    {
+        name: 'Snickers',
+        flavor: 'chocolate caramel nougat',
+        type: 'candy bar'
+    }
+    {
+        name: 'Sour Patch Kids',
+        flavor: 'sweet and sour',
+        type: 'gummy candy'
+    } 
+];
